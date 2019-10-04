079299890b82dd780fd3403fec9b92f648696e73
Tyler Barnes <tylerdbarnes@gmail.com>
2019-10-04 13:34:31 +0100

    add apollo link and client

---
+++ plugins/gatsby-source-wpgraphql/create-client.js
@@ -0,0 +1,17 @@
+const { createHttpLink } = require(`apollo-link-http`)
+
+exports.createClient = async ({ createLink }) => {
+    let link
+    if (createLink) {
+        link = await createLink(options)
+    } else {
+        link = createHttpLink({
+        uri: url,
+        fetch,
+        headers,
+        fetchOptions,
+        })
+    }
+
+    return link;
+}
