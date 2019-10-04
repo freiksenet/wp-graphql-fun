62ae6c2d6e2772c951b67e111b37c0d04be3f560
Tyler Barnes <tylerdbarnes@gmail.com>
2019-10-04 13:41:39 +0100

    add introspection with graphql-tools

--- plugins/gatsby-source-wpgraphql/gatsby-node.js
+++ plugins/gatsby-source-wpgraphql/gatsby-node.js
@@ -1,4 +1,5 @@
 const { createClient } = require("./create-client")
+const { introspectSchema } = require(`graphql-tools`)
 
 const DEFAULT_TYPE_CONFIG = {
   MediaType: {
@@ -17,7 +18,7 @@ exports.onPreBootstrap = () => {
 }
 
 exports.createSchemaCustomizatino = async ({ actions, options }) => {
-  const introspection = await doIntrospectionQueryTODO()
+  const introspection = await introspectSchema(CLIENT)
   const typesToCreate = Object.keys(options.types)
   for (const type of typesToCreate) {
     const typeIntrospection = introspection.data.__schema.types.find(
